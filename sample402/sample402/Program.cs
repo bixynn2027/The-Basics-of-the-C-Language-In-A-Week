ef7331bf215f7417300bcf42b1300cc78d1aa9b8
bixynn2027 <59707557+bixynn2027@users.noreply.github.com>
2020-01-27 23:46:02 +0900

    for文の2重ループ構造の作成

---
+++ sample402/sample402/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample402
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 2; i++){
+                for(int j = 1; j <= 3; j++){
+                    int k = i + j;
+                    Console.WriteLine(i+"+"+j+"="+k+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
